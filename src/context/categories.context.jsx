03e8a44915208b40d6a81b7d8964c22a29f8e825
Aaron Trovillion <aaron.trovillion@gmail.com>
2024-05-20 10:31:48 -0400

    Built out individual category routes and created previews of categories on shop page

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from 'react';
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+
+// import SHOP_DATA from '../shop-data';
+
+// actual value that will be accessed
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+// products provider component
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  console.log(categoriesMap);
+
+  useEffect(() => {
+    // we create our own, separate async callback function inside the useEffect hook...
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments('categories');
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  // eslint-disable-next-line react/jsx-no-constructed-context-values
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
