74b82dc9dc4902d9735f93ea41358e4e2c7c09b7
Aaron Trovillion <aaron.trovillion@gmail.com>
2024-05-22 14:35:41 -0400

    Converted useState hooks in context to reducers for state management

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 import {
   onAuthStateChangedListener,
@@ -11,11 +13,39 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-// user provider component--allows any component wrapped in provider element to access
-// the value(s) passed into it
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+  console.log('dispatched');
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
 
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  };
+
   // pass object containing the two important values that need to be accessed
   // eslint-disable-next-line react/jsx-no-constructed-context-values
   const value = { currentUser, setCurrentUser };
